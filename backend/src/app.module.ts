de1eaacb87cd1ab8cef745a58329140be0ae6edf
Markus Persson <markusperssonbiz@gmail.com>
2022-05-20 17:37:32 +0200

    Initialize project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './modules/auth/auth.controller';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseService } from './modules/database/database.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [AuthController],
-  providers: [],
+  providers: [DatabaseService],
 })
 export class AppModule {}
